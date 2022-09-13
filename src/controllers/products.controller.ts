5b465927458e390bbc7f830506f804c1dd4b53da
Igor Leal <igorhenriqueleal3@gmail.com>
2022-09-13 18:41:37 -0300

    feat: Criado o método getAll no controller products

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -5,6 +5,11 @@ import ProductsService from '../services/products.service';
 export default class ProductsController {
   constructor(private productsService = new ProductsService()) { }
 
+  public getAll = async (_req: Request, res: Response) => {
+    const products = await this.productsService.getAll();
+    res.status(StatusCodes.OK).json(products);
+  };
+
   public create = async (req: Request, res: Response) => {
     const products = req.body;
     const productsCreated = await this.productsService.create(products);
