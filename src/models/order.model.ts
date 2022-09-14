2f138081a727bb46b0f305c2fcde097510ebd4c2
Igor Leal <igorhenriqueleal3@gmail.com>
2022-09-14 10:04:39 -0300

    feat: adicionado o alias productsIds na query do model Order

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -5,7 +5,7 @@ import IOrder from '../interfaces/order.interface';
 const OrderModel = {
   getAll: async () => {
     const query = `
-    SELECT orders.id, orders.userId, JSON_ARRAYAGG(products.id)
+    SELECT orders.id, orders.userId, JSON_ARRAYAGG(products.id) AS productsIds
     FROM Trybesmith.Orders As orders
     INNER JOIN Trybesmith.Products As products
     ON orders.id = products.orderId
