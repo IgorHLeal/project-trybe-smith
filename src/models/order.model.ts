bab8af3e58636f8d0780ddb8615294ddc3f12f7e
Igor Leal <igorhenriqueleal3@gmail.com>
2022-09-14 09:36:13 -0300

    feat: Criado o método getAll no model Order

---
+++ src/models/order.model.ts
@@ -0,0 +1,22 @@
+import { RowDataPacket } from 'mysql2';
+import connection from './connection';
+import IOrder from '../interfaces/order.interface';
+
+const OrderModel = {
+  getAll: async () => {
+    const query = `
+    SELECT orders.id, orders.userId, JSON_ARRAYAGG(products.id)
+    FROM Trybesmith.Orders As orders
+    INNER JOIN Trybesmith.Products As products
+    ON orders.id = products.orderId
+    GROUP BY orders.id
+    ORDER BY orders.userId
+    `;
+
+    const [result] = await connection.execute<RowDataPacket[]>(query);
+
+    return result as unknown as IOrder;
+  },
+};
+
+export default OrderModel;
