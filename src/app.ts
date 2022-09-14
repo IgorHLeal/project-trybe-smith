78c1fd7ab97aa5f45153e4a0bb0b867f4807547b
Igor Leal <igorhenriqueleal3@gmail.com>
2022-09-14 09:55:39 -0300

    feat: Criada a rota GET para o endpoint /orders

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import ProductsRouter from './routes/products.routes';
 import UsersRouter from './routes/users.routes';
+import OrderRouter from './routes/order.routes';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use('/products', ProductsRouter);
 app.use('/users', UsersRouter);
+app.use('/orders', OrderRouter);
 
 export default app;
