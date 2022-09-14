a81133922301ee029a0783e873ccde0b36b48a5b
Igor Leal <igorhenriqueleal3@gmail.com>
2022-09-14 08:09:23 -0300

    feat: Criado o método create no model Users

---
+++ src/models/users.model.ts
@@ -0,0 +1,18 @@
+import { ResultSetHeader } from 'mysql2';
+import connection from './connection'
+import IUser from '../interfaces/users.interface';
+
+const UsersModel = {
+  create: async ({ username, classe, level, password }: IUser): Promise<number> => {
+    const query = `INSERT INTO Trybesmith.Users (username, classe, level, password),
+      VALUES (?, ?, ?, ?)`;
+
+    const values = [username, classe, level, password];
+
+    const [{ insertId }] = await connection.execute<ResultSetHeader>(query, values);
+
+    return insertId;
+  },
+};
+
+export default UsersModel; 
